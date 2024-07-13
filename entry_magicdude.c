dcb118e0090e80afb0ebeb3a2454d840fb5c6891
Franco Schillage (Tipo) <fschillage@gmail.com>
2024-07-13 20:36:43 -0300

    Add basic world with player and rocks

---
+++ entry_magicdude.c
@@ -0,0 +1,276 @@
+// Something about tiles
+
+const int tile_width = 8;
+
+int world_pos_to_tile_pos(float world_pos) {
+	return roundf(world_pos / (float)tile_width);
+}
+
+float tile_pos_to_world_pos(int tile_pos) {
+	return ((float)tile_pos * (float)tile_width);
+}
+
+
+// Animation Camera thingy?
+
+bool almost_equals(float a, float b, float epsilon) {
+ return fabs(a - b) <= epsilon;
+}
+
+bool animate_f32_to_target(float* value, float target, float delta_t, float rate) {
+	*value += (target - *value) * (1.0 - pow(2.0f, -rate * delta_t));
+	if (almost_equals(*value, target, 0.001f))
+	{
+		*value = target;
+		return true; // reached
+	}
+	return false;
+}
+
+void animate_v2_to_target(Vector2* value, Vector2 target, float delta_t, float rate) {
+	animate_f32_to_target(&(value->x), target.x, delta_t, rate);
+	animate_f32_to_target(&(value->y), target.y, delta_t, rate);
+}
+
+// Sprites
+typedef struct Sprite {
+	Gfx_Image* image;
+} Sprite;
+
+typedef enum SpriteID {
+	SPRITE_nil,
+	SPRITE_player,
+	SPRITE_rock0,
+	SPRITE_MAX,
+} SpriteID;
+
+Sprite sprites[SPRITE_MAX];
+Sprite* get_sprite(SpriteID id) {
+	if (id >= 0 && id < SPRITE_MAX) {
+		return &sprites[id];
+	}
+	return &sprites[0];
+}
+
+Vector2 get_sprite_size(Sprite* sprite) {
+	return (Vector2) { sprite->image->width, sprite->image->height };
+}
+
+// Entities
+
+typedef enum EntityArchetype {
+	arch_nil = 0,
+	arch_rock = 1,
+	arch_tree = 2,
+	arch_player = 3,
+	arch_item_rock = 4,
+	arch_item_pine_wood = 5,
+	ARCH_MAX,
+} EntityArchetype;
+
+typedef struct Entity {
+	bool is_valid;
+	EntityArchetype arch;
+	Vector2 pos;
+	bool render_sprite;
+	SpriteID sprite_id;
+	int health;
+	bool destroyable_world_item;
+	bool is_item;
+} Entity;
+
+
+#define MAX_ENTITY_COUNT 1024
+
+// World
+
+typedef struct World {
+	Entity entities[MAX_ENTITY_COUNT];
+} World;
+World* world = 0;
+
+Entity* entity_create() {
+	Entity* entity_found = 0;
+	for (int i = 0; i < MAX_ENTITY_COUNT; i++) {
+		Entity* existing_entity = &world->entities[i];
+		if (!existing_entity->is_valid) {
+			entity_found = existing_entity;
+			break;
+		}
+	}
+	assert(entity_found, "No more free entities!");
+	entity_found->is_valid = true;
+	return entity_found;
+}
+
+void entity_destroy(Entity* entity) {
+	memset(entity, 0, sizeof(Entity));
+}
+
+void setup_player(Entity* en) {
+	en->arch = arch_player;
+	en->sprite_id = SPRITE_player;
+}
+
+void setup_rock(Entity* en) {
+	en->arch = arch_rock;
+	en->sprite_id = SPRITE_rock0;
+	// en->health = rock_health;
+	// en->destroyable_world_item = true;
+}
+
+Vector2 screen_to_world() {
+	float mouse_x = input_frame.mouse_x;
+	float mouse_y = input_frame.mouse_y;
+	Matrix4 proj = draw_frame.projection;
+	Matrix4 view = draw_frame.view;
+	float window_w = window.width;
+	float window_h = window.height;
+
+	// Normalize the mouse coordinates
+	float ndc_x = (mouse_x / (window_w * 0.5f)) - 1.0f;
+	float ndc_y = (mouse_y / (window_h * 0.5f)) - 1.0f;
+
+	// Transform to world coordinates
+	Vector4 world_pos = v4(ndc_x, ndc_y, 0, 1);
+	world_pos = m4_transform(m4_inverse(proj), world_pos);
+	world_pos = m4_transform(view, world_pos);
+	// log("%f, %f", world_pos.x, world_pos.y);
+
+	// Return as 2D vector
+	return (Vector2){ world_pos.x, world_pos.y };
+}
+
+int entry(int argc, char **argv) {
+	
+	window.title = STR("The hazardous adventures of magic dude");
+	window.scaled_width = 1280; // We need to set the scaled size if we want to handle system scaling (DPI)
+	window.scaled_height = 720; 
+	window.x = 200;
+	window.y = 90;
+	window.clear_color = hex_to_rgba(0xBEFD7Fff);
+
+	world = alloc(get_heap_allocator(), sizeof(World));
+	memset(world, 0, sizeof(World));
+
+	sprites[SPRITE_player] = (Sprite){ .image=load_image_from_disk(STR("res/sprites/player.png"), get_heap_allocator()) };
+	sprites[SPRITE_rock0] = (Sprite){ .image=load_image_from_disk(STR("res/sprites/rock0.png"), get_heap_allocator()) };
+
+	Entity* player_en = entity_create();
+	setup_player(player_en);
+
+	for (int i = 0; i < 10; i++) {
+		Entity* en = entity_create();
+		setup_rock(en);
+		en->pos = v2(get_random_float32_in_range(-200, 200), get_random_float32_in_range(-200, 200));
+		// en->pos = round_v2_to_tile(en->pos);
+		// en->pos.y -= tile_width * 0.5;
+	}
+	
+	Vector2 player_pos = v2(0, 0);
+	float64 seconds_counter = 0.0;
+	s32 frame_count = 0;
+	float64 last_time = os_get_current_time_in_seconds();
+	float zoom = 5.3;
+	Vector2 camera_pos = v2(0, 0);
+
+	while (!window.should_close) {
+		reset_temporary_storage();
+
+		float64 now = os_get_current_time_in_seconds();
+		float64 delta_t = now - last_time;
+		last_time = now;
+
+		draw_frame.projection = m4_make_orthographic_projection(window.width * -0.5, window.width * 0.5, window.height * -0.5, window.height * 0.5, -1, 10);
+
+		// Camera
+		{
+			Vector2 target_pos = player_en->pos;
+			animate_v2_to_target(&camera_pos, target_pos, delta_t, 30.0f);
+
+			draw_frame.view = m4_make_scale(v3(1.0, 1.0, 1.0));
+			draw_frame.view = m4_mul(draw_frame.view, m4_make_translation(v3(camera_pos.x, camera_pos.y, 0)));
+			draw_frame.view = m4_mul(draw_frame.view, m4_make_scale(v3(1.0/zoom, 1.0/zoom, 1.0)));
+		}
+
+		// :Render
+				for (int i = 0; i < MAX_ENTITY_COUNT; i++) {
+			Entity* en = &world->entities[i];
+			if (en->is_valid) {
+
+				switch (en->arch) {
+
+					default:
+					{
+						Sprite* sprite = get_sprite(en->sprite_id);
+						Matrix4 xform = m4_scalar(1.0);
+						// if (en->is_item) {
+						// 	xform         = m4_translate(xform, v3(0, 2.0 * sin_breathe(os_get_current_time_in_seconds(), 5.0), 0));
+						// }
+						xform         = m4_translate(xform, v3(0, tile_width * -0.5, 0));
+						xform         = m4_translate(xform, v3(en->pos.x, en->pos.y, 0));
+						xform         = m4_translate(xform, v3(get_sprite_size(sprite).x * -0.5, 0.0, 0));
+
+						Vector4 col = COLOR_WHITE;
+						// if (world_frame.selected_entity == en) {
+						// 	col = COLOR_RED;
+						// }
+
+						draw_image_xform(sprite->image, xform, get_sprite_size(sprite), col);
+
+						// debug pos 
+						// draw_text(font, sprint(temp, STR("%f %f"), en->pos.x, en->pos.y), font_height, en->pos, v2(0.1, 0.1), COLOR_WHITE);
+
+						break;
+					}
+				}
+			}
+		}
+		// Sprite* sprite = get_sprite(player_en->sprite_id);
+
+		// Matrix4 xform = m4_scalar(1.0);
+		// xform         = m4_translate(xform, v3(player_en->pos.x, player_en->pos.y, 0));
+		// draw_image_xform(sprite->image, xform, get_sprite_size(sprite), COLOR_WHITE);
+
+
+		// Movement
+
+		if (is_key_just_pressed(KEY_ESCAPE)) {
+			window.should_close = true;
+		}
+
+		Vector2 input_axis = v2(0, 0);
+		if (is_key_down('A')) {
+			input_axis.x -= 1.0;
+		}
+		if (is_key_down('D')) {
+			input_axis.x += 1.0;
+		}
+		if (is_key_down('S')) {
+			input_axis.y -= 1.0;
+		}
+		if (is_key_down('W')) {
+			input_axis.y += 1.0;
+		}
+		input_axis = v2_normalize(input_axis);
+
+		player_en->pos = v2_add(player_en->pos, v2_mulf(input_axis, 100.0 * delta_t));
+
+		// Debug movement
+		// if (player_en->pos.x > 0.0 || player_en->pos.y > 0.0) {
+		// 	printf("the fucker is moving\n");
+		// }
+
+		os_update(); 
+		gfx_update();
+		seconds_counter += delta_t;
+		frame_count += 1;
+		if (seconds_counter > 1.0) {
+			log("fps: %i", frame_count);
+			seconds_counter = 0.0;
+			frame_count = 0;
+		}
+	}
+
+	return 0;
+}
